95035b68c845b96ded1465e419fc410301a61399
S0AndS0 <strangerthanbland@gmail.com>
2019-08-29 15:46:19 -0700

    :bug: Corrects `drag_absolute` and `drag_relative` methods

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 
+import time
+
 from Xlib.display import Display
 from Xlib import X
 from Xlib.ext.xtest import fake_input
@@ -28,17 +30,31 @@ class XMouse_Remote(object):
     """
     Python2/3 mouse wrapper API of `Xlib`
 
-        mouse = XMouse_Remote(display = ':0')
+    - `display` optional if `DISPLAY` environment variable is set
+    - `button_ids` optional if satisfied with defaults
+
+    ## Example usage
+
+        mouse = XMouse_Remote(display = ':0', button_ids = {
+            'button_left': 1,
+            'button_middle': 2,
+            'button_right': 3,
+            'scroll_up': 4,
+            'scroll_down': 5,
+            'scroll_left': 6,
+            'scroll_right': 7
+        })
 
         print("XMouse_Remote location -> {}".format(mouse.location))
-        mouse.move_absolute(x = 5, y = 5)
+        mouse.move_relative(x = 5)
+        print("XMouse_Remote location -> {}".format(mouse.location))
 
-    Note for script writers, most of the logics are in the following methods
+    > Note for script writers, most of the logics are in the following methods
 
-    - button_press
-    - button_release
-    - move_absolute
-    - scroll
+    - `button_press`
+    - `button_release`
+    - `move_absolute`
+    - `scroll`
     """
 
     def __init__(self, display = None, button_ids = None):
@@ -76,48 +92,57 @@ class XMouse_Remote(object):
         self._coordinates = self.display.screen().root.query_pointer()._data
         return [self._coordinates.get('root_x'), self._coordinates.get('root_y')]
 
-    def button_click(self, x, y, detail = 1, button_name = None, times = 1, sync = True):
+    def button_click(self, detail = 1, button_name = None, times = 1, sync = True, delays = {0: 0.01}):
         """
-        Calls `self.button_press(...)` and `self.button_release(...)` before returning `self.location`
+        Calls `self.button_press(...)` and `self.button_release(...)`
         """
         for _ in range(times):
-            self.button_press(x = x, y = y, detail = detail, button_name = button_name, sync = sync)
-            self.button_release(x = x, y = y, detail = detail, button_name = button_name, sync = sync)
+            self.button_press(detail = detail, button_name = button_name, sync = sync)
 
-        return self.location
+            if delays.get(0, 0) > 0:
+                time.sleep(delays[0])
 
-    def drag_absolute(self, x, y, detail = 1, button_name = None, sync = True):
+            self.button_release(detail = detail, button_name = button_name, sync = sync)
+
+    def drag_absolute(self, x, y, detail = 1, button_name = None, sync = True, delays = {0: 0.01, 1: 0.01}):
         """
         Starting from `self.location`, moves to absolute coordinates while pressing defined button IDs
         """
-        self.button_press(*self.location, detail = detail, button_name = button_name, sync = False)
-        self.move_absolute(x = x, y = y, sync = False)
-        self.button_release(x = x, y = y, detail = detail, button_name = button_name, sync = False)
+        self.button_press(detail = detail, button_name = button_name, sync = sync)
 
-        if sync:
-            self.display.sync()
+        if delays.get(0, 0) > 0:
+            time.sleep(delays[0])
+
+        self.move_absolute(x = x, y = y, sync = sync)
+
+        if delays.get(1, 0) > 0:
+            time.sleep(delays[1])
+
+        self.button_release(detail = self._target_id, sync = sync)
 
         return self.location
 
-    def drag_relative(self, x, y, detail = 1, button_name = None, sync = True):
+    def drag_relative(self, x = 0, y = 0, detail = 1, button_name = None, sync = True, delays = {0: 0.01, 1: 0.01}):
         """
         Starting from `self.location`, moves to relative coordinates while pressing defined button IDs
         """
-        self.button_press(*self.location, detail = detail, button_name = button_name, sync = False)
-        self.move_relative(x = x, y = y, sync = False)
-        self.button_release(x = x, y = y, detail = detail, button_name = button_name, sync = False)
+        self.button_press(detail = detail, button_name = button_name, sync = sync)
 
-        if sync:
-            self.display.sync()
+        if delays.get(0, 0) > 0:
+            time.sleep(delays[0])
+
+        self.move_relative(x = x, y = y, sync = sync)
+
+        if delays.get(1, 0) > 0:
+            time.sleep(delays[1])
+
+        self.button_release(detail = detail, button_name = button_name, sync = sync)
 
         return self.location
 
-    def button_press(self, x, y, detail = 1, button_name = None, sync = True):
+    def button_press(self, detail = 1, button_name = None, sync = True):
         """
-        Returns `self.location` after telaporting and pressing button ID or name
         """
-        self.move_absolute(x = x, y = y, sync = sync)
-
         self._target_id = detail
         if button_name is not None:
             self._target_id = self.button_ids.get(button_name, 1)
@@ -127,14 +152,9 @@ class XMouse_Remote(object):
         if sync:
             self.display.sync()
 
-        return self.location
-
-    def button_release(self, x, y, detail = 1, button_name = None, sync = True):
+    def button_release(self, detail = 1, button_name = None, sync = True):
         """
-        Returns `self.location` after telaporting and releasing button ID or name
         """
-        self.move_absolute(x = x, y = y, sync = sync)
-
         self._target_id = detail
         if button_name is not None:
             self._target_id = self.button_ids.get(button_name, 1)
@@ -143,8 +163,6 @@ class XMouse_Remote(object):
 
         if sync:
             self.display.sync()
-
-        return self.location
 
     def move_absolute(self, x, y, sync = True):
         """
@@ -163,6 +181,9 @@ class XMouse_Remote(object):
     def move_relative(self, x = 0, y = 0, sync = True):
         """
         Returns `self.location` after moving relative distance from last coordinates
+
+        - If positive moves cursor Up or Left
+        - If negative moves cursor Down or Right
         """
         self._new_location = self.location
 
